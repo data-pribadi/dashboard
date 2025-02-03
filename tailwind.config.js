9ef617721866bdc547553ffa0202178cf56cb0d8
useroot.id <useroot.id@gmail.com>
2025-02-03 08:48:13 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html",
-      "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      require('flowbite/plugin')
-    ],
-  }
-  
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+}
